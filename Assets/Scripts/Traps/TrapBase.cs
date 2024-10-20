7f62d0793a78ccede763ed98dad2e4b76497b2f6
Dat Doan <dat.doantrongk57@gmail.com>
2024-10-20 17:50:37 +0700

    + Add trap base for all trap class + Add trap land slide

---
+++ Assets/Scripts/Traps/TrapBase.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TrapBase : MonoBehaviour
+{
+    // Public
+    public string trapName = string.Empty;
+    public GameObject trapObject;
+    public float activationDelay = 0;
+    // Private
+    public bool isActivated = false;
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isActivated && other.CompareTag("Player"))
+        {
+            // Player has entered the trigger
+            StartCoroutine(ActivateTrap());
+        }
+    }
+
+    protected IEnumerator ActivateTrap()
+    {
+        isActivated = true;
+        // Add your trap activation logic here, e.g., changing appearance, triggering animations, applying forces
+        yield return new WaitForSeconds(activationDelay);
+        // Add your trap deactivation logic here, if needed
+        PlayTrap();
+        isActivated = false;
+    }
+
+    public abstract void PlayTrap(); 
+}
---
+++ Assets/Scripts/Traps/TrapLandSlide.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrapLandSlide : TrapBase
+{
+    public override void PlayTrap()
+    {
+        Debug.Log("On PlayTrap");
+    }
+}
