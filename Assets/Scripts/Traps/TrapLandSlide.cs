a048ef44531462ea17ab888ed2448cbdf57b168d
Dat Doan <dat.doantrongk57@gmail.com>
2024-10-21 17:23:45 +0700

    + Update code for TrapLandSlide

--- Assets/Scripts/Traps/TrapLandSlide.cs
+++ Assets/Scripts/Traps/TrapLandSlide.cs
@@ -3,11 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TrapLandSlide : TrapBase
 {
     public override void PlayTrap()
     {
         Debug.Log("On PlayTrap");
+        //tilemap.SetTile(removedPostion, null);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        StartCoroutine(DestroyTrap());
+    }
+
+    private IEnumerator DestroyTrap()
+    {
+        Debug.Log("DestroyTrap");
+        yield return new WaitForSeconds(5f);
+        Debug.Log("DestroyTrap Done");
+        Destroy(gameObject);
     }
 }
